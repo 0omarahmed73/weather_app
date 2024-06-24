bce756fe5f52881e6df2bb2b4c70a012b118c89e
Omar Ahmed <0omarahmed73@gmail.com>
2024-06-25 00:16:45 +0300

    format time to be AM/PM

--- src/components/DateTheme.jsx
+++ src/components/DateTheme.jsx
@@ -1,20 +1,38 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Col } from "react-bootstrap";
 import useLocalStorage from "use-local-storage";
 import { WeatherContext } from "../contexts/WeatherContext";
 
 export function DateTheme({ col = 2 }) {
   const [theme, setTheme] = useLocalStorage("theme", "light");
-  const {weather} = useContext(WeatherContext);
+  const { weather } = useContext(WeatherContext);
   const handleThemeToggle = () => {
     setTheme(theme === "light" ? "dark" : "light");
+  };
+  let [date, setDate] = useState(new Date());
+  useEffect(() => {
+    if (weather) {
+      // Correctly create a new Date object using the timestamp
+      setDate(new Date(weather.location.localtime_epoch * 1000));
+    }
+  }, [weather]);
+
+  const formatTime = (date) => {
+    if (!(date instanceof Date)) return "";
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let ampm = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? `0${minutes}` : minutes; // add leading zero if needed
+    return `${hours}:${minutes} ${ampm}`;
   };
 
   return (
     <Col xs={col} className="date-theme">
       <div className="date-time">
-        <p>{weather ? weather.location.localtime.split(' ')[0] : ''}</p>
-        <p>{weather ? weather.location.localtime.split(' ')[1] : ''}</p>
+        <p>{weather ? weather.location.localtime.split(" ")[0] : ""}</p>
+        <p>{weather ? formatTime(date) : ""}</p>{" "}
       </div>
       <label className="switch">
         <input
